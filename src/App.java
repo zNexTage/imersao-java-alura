5d81be9af13e095c803ba017080386f494f1b932
Christian Oliveira <christianoliveira1908@hotmail.com>
2022-07-20 21:13:52 -0300

    Refatoração do metódo Main da aplicação.

--- src/App.java
+++ src/App.java
@@ -1,44 +1,50 @@
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.util.Collection;
 import Entities.Movie;
-import Entities.MovieResponse;
-import Services.GetImdbMovies;
+import Entities.NasaApod;
+import HttpClient.HttpMovies.GetImdbTop250Movies;
+import HttpClient.HttpMovies.GetNasaApod;
+import HttpClient.Providers.GetClient;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /* 
-            1 - Fazer uma conexão HTTP e buscar os top 250 filmes;
-            2 - Pegar só os dados que interessam (título, poster, classificação)
-            3 - Exibir e manipular os dados
-        */
+        getMovies();        
+    }
 
-        //1:
-        String moviesJson = new GetImdbMovies().getMovies();
+    public static void getMovies() throws IOException, InterruptedException{
+        Collection<Movie> moviesJson = new GetImdbTop250Movies(new GetClient()).get();  
 
-        //2:
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-
-        //List<Movie> moviesResponse = objectMapper.readValue(moviesJson, new TypeReference<List<Movie>>() {});        
-        MovieResponse moviesResponse = objectMapper.readValue(moviesJson, MovieResponse.class);        
-
-        //3:
         StickerFactory sticker = new StickerFactory();
 
-        for (Movie movie : moviesResponse.getItems()) {
+        for (Movie movie : moviesJson) {
             System.out.println(movie.getTitle());
             System.out.println(movie.getImage());
-            System.out.println(movie.imDbRating());
+            System.out.println((int)movie.imDbRating());
             
             InputStream fileUrl = new URL(movie.getImage()).openStream();
 
             sticker.create(fileUrl, movie.getTitle());
+
+            System.out.println("--------------------------------------");
+        }
+    }
+
+    public static void getNasaApod() throws IOException, InterruptedException{
+        Collection<NasaApod> nasaApods = new GetNasaApod(new GetClient()).get("2022-07-10", "2022-07-20");  
+
+        StickerFactory sticker = new StickerFactory();
+
+        for (NasaApod nasaApod : nasaApods) {
+            System.out.println(nasaApod.getTitle());
+            //System.out.println(nasaApod.getExplanation());
+            
+            InputStream fileUrl = new URL(nasaApod.getUrl()).openStream();
+
+            sticker.create(fileUrl, nasaApod.getTitle());
+
+            System.out.println("--------------------------------------");
         }
     }
 }
